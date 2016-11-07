a269302f6d1852d430cf66bfa876bd85f225c9b0
Abhinav Vishwa <vishwaa@deshaw.com>
2016-11-07 21:38:07 +0530

    Remove redundant files

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function init() {
-  const XKCD = 'http://xkcd.com/';
-  fetch(`${XKCD}info.0.json`, {
-      credentials: 'include',
-      mode: 'no-cors'
-    })
-    .then(response => response.json())
-    .then(json => console.log(json));
-})();
