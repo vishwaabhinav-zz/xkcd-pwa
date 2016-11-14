838e7a4130c83181edcc1f67ee56f7f48e1f25a0
Abhinav Vishwa <vishwaa@deshaw.com>
2016-11-14 12:07:04 +0530

    Add db connection string

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const XKCD = 'http://xkcd.com/';
-
+const dbstr = 'mongodb://heroku_97mjvv9b:l7qh0eg92ln8echsl6no1e6en0@ds145997.mlab.com:45997/heroku_97mjvv9b';
 app.use(express.static('static'))
 
 app.get('/', (req, res) => {
@@ -19,6 +19,10 @@ app.get('/latest', (req, res) => {
       res.json(body);
     }
   });
+});
+
+app.get('/all', (req, res) => {
+
 });
 
 app.listen(PORT, () => {
